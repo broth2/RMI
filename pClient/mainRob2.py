66b1fe3f01b773832c1585dfb1ac9f845c28a72c
João Castanheira <joao@pop-os.localdomain>
2023-10-18 01:30:05 +0100

    deste lado faz  mais

--- pClient/mainRob2.py
+++ pClient/mainRob2.py
@@ -146,7 +146,6 @@ class MyRob(CRobLinkAngs):
 
             self.get_orientation()
             coord = (x,y)
-
             #visited_orientations_coord = self.get_visited_orientations(coord)
             #print("visited orientations:", visited_orientations_coord)
             if coord == (self.new_x, self.new_y):
@@ -207,8 +206,8 @@ class MyRob(CRobLinkAngs):
 
                     print("visited orientations:", self.visited_orientation)
                     print("X:",x, " Y:",y)
-                    x = int(x/2)
-                    y = int(y/2)
+                    x = int(round(x)/2)
+                    y = int(round(y)/2)
                     # generic case adapt for the first one
                     # TODO instead of '-1' must be 0 or 1, the values must already be known
                     #curr_x = 0
@@ -256,8 +255,7 @@ class MyRob(CRobLinkAngs):
                         #for row in self.state:
                         #    print(" ".join(str(cell) if cell is not None else "None" for cell in row))
 
-                # clear vars
-                self.paths = []
+                #pass
 
 
     def rotate_to_orientation(self, possible_paths):
@@ -283,6 +281,8 @@ class MyRob(CRobLinkAngs):
             print("stop")
             self.driveMotors(0,-0)
             self.new_orientation = True
+            print("new_orientation:", self.new_orientation)
+            self.has_new_coords = False
         
 
 
@@ -290,7 +290,7 @@ class MyRob(CRobLinkAngs):
         #print mygps with 2 decimal places
         print("myX:",round(x-self.start_x,2), " myY:",round(y-self.start_y,2),"\n")
         print("newX:",self.new_x, " newY:",self.new_y,"\n")
-        return (round(x-self.start_x), round(y-self.start_y))
+        return (x-self.start_x, y-self.start_y)
             
     def get_orientation(self):
 
